a87006b5ff0df5e86ef20dec1145f2f7cd091799
JasonHaque <zahinjason220434@gmail.com>
2019-10-29 11:54:58 +0600

    variable test

---
+++ src/variables.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val name :String ="Jason"
+    var question ="Who wrote the code ?"
+    println(question)
+    println(name)
+    question ="Did he really?"
+    println(question)
+    val answerBinary:Boolean = true
+    println(answerBinary)
+}
